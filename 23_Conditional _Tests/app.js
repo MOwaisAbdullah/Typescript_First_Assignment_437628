fa06be8483bf1e436bd4a59a5c3df8d63821e79e
M.Owais Abdullah <owaisgold12@gmail.com>
2024-03-10 14:55:50 +0500

    added folders 23 to 29

---
+++ 23_Conditional _Tests/app.js
@@ -0,0 +1,41 @@
+"use strict";
+//Test # 1
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//Test # 2
+let bus = 'greenline';
+console.log("Is bus === 'greenline'? I predict True.");
+console.log(bus === 'greenline');
+//Test # 3
+let a = '2';
+console.log("Is '2' === '2'? I predict True.");
+console.log(a === '2');
+//Test # 4
+let b = 'Australia';
+console.log("Is Australia == australia? I predict False.");
+console.log(b == 'australia');
+//Test # 5
+let c = 'Australia';
+console.log("Is c == 'Australia'? I predict True.");
+console.log(c == 'Australia');
+//Test # 6
+let d = 5;
+console.log("Is 5 > 10? I predict false.");
+console.log(d > 10);
+//Test # 7
+let e = 5;
+console.log("Is 5 < 10? I predict true.");
+console.log(e < 10);
+//Test # 8
+let f = 15;
+console.log("Is 15 <= 10? I predict false.");
+console.log(f <= 10);
+//Test # 9
+let g = 6;
+console.log("Is 5 => 10? I predict false.");
+console.log(g >= 10);
+//Test # 10
+let h = 19;
+console.log("Is 19 != 19? I predict false.");
+console.log(h != 19);
---
+++ 24_More_Conditional_Tests/app.ts
@@ -0,0 +1,35 @@
+let text ="i am fine."
+console.log(text == "I Am Fine")
+
+let a = "Canada"
+console.log(a == "canada");
+
+let b = "italy"
+console.log(b == "italy");
+
+let x = 25
+console.log(x > 10 && x < 100)
+
+
+let y = 125
+let z = 300
+
+console.log(y > 200 || z < 500)
+let list: string[]=['Uroosa','Farah','Sufiyan','Bilal']
+console.log(list.includes('Bilal'))
+console.log(list.includes('farah'))
+
+let d = 5;
+
+console.log(d > 10)
+let e = 5;
+
+console.log(e < 10)
+let f = 15;
+console.log(f <= 10)
+
+let g = 6;
+console.log(g >= 10)
+
+let h = 19 ;
+console.log(h !=19)
